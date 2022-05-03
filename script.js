92821fb08e8355f51b5dd2cb28ffdbbac5771051
Madan <madanbastakoti5@gmail.com>
2022-05-03 18:53:08 +0545

    Simple Interest Calculator

--- script.js
+++ script.js
@@ -1,107 +1,16 @@
-/*console.log("hello World!");
+// const principal = 1000;
+// const time = 2;
+// const rate = 25;
 
-//data types in javascripts
-//operator precedence
+// const simpleInterest = (principal*time*rate)/100;
 
-//Dynamic Typed
-
-let countryName = "Nepal"; //camel-case convention e.g.countryName
-console.log(countryName);
-console.log(typeof countryName);
-
-countryName = 88;
-console.log(countryName);
-console.log(typeof countryName);
-
-//let -> we can change later in the runtime
-//const -> we cannot change later in the runtime
-
-const personName = "Madan Bastakoti";
-console.log(personName);
-
-// personName = "Dikshak Poudel";
-
-//data types in javascript
-// 1. Number
-// 2. String
-// 3. Object
-// 4. Boolean
-// 5. List
-// 6. undefined
-// 7. null
-
-const scoreInt = 78;
-const schoolName = "SBS";
-const seeMarks = 798.33;
-const isValid = false;
-const isLoggedin = true;
-
-const nullVal = null;
-let myName;
-
-console.log(
-  typeof scoreInt,
-  typeof schoolName,
-  typeof seeMarks,
-  typeof isValid,
-  typeof isLoggedin
-);
-
-console.log(typeof nullVal, typeof myName);
-
-//EVERYTHING IN JAVASCRIPT IS OBJECT.
-
-//Operators in Javascripts +, -, *, /, %,  ++, --, +=, -=, /=, (), **, %=   //Arithemetic Operators
-let num1 = 30;
-let num2 = 20;
-let num3;
-num3 = num1 + num2;
-
-console.log(num3, typeof num3);
-
-num3 = num1 - num2;
-console.log(num3);
-
-num3 = num1 * num2;
-console.log(num3);
-
-num3 = num1 / num2;
-console.log(num3);
-
-console.log("value of num3: " + num3);
-num3++;
-console.log("After the increment: " + num3);
-
-console.log("value of num3: " + num3);
-num3--;
-console.log("After the decrement: " + num3);
-*/
-
-//unary operators  +=, -=, *=, /=
+// console.log("The Simple Interest is: " + simpleInterest.toFixed(2));
 
 
-num1 = 15;
-num2 = 35;
+const principal = 5000;
+const time = 2.5;
+const rate = 25.3;
 
-// num1 = num1 + 10; num1 += 10; both are same
-num1 += 10;
+const simpleInterest = (principal*time*rate)/100;
 
-console.log("value of num1 (+=): "+  num1);
-
-num1 -= 7;
-console.log("value of num1 (-=): "+  num1);
-
-num1 *= 7;
-console.log("value of num1 (*=): "+  num1);
-
-num1 /= 7;
-console.log("value of num1 (/=): "+  num1);
-
-
-console.log((9+2)/7);
-
-console.log(5**2);
-console.log(3**3);
-
-//Logical Operators
-// &&, ||, !
+console.log("The Simple Interest is: $" + simpleInterest.toFixed(2));
