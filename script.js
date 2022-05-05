a893ea7ac947f4d9aa8020d27364756cc178a323
Madan <madanbastakoti5@gmail.com>
2022-05-05 19:19:10 +0545

    homework for the if else statement

--- script.js
+++ script.js
@@ -130,3 +130,31 @@ console.log(3 ** 3);
 console.log(2 > 3 && 3 < 4);
 
 console.log(2 < 1 || 4 < 6);
+
+//conditional statement
+if (3 > 4) {
+  console.log("This will not not get executed");
+} else if (5 < 7) console.log("5<7 is always right");
+else console.log("This will get executed if both are not true");
+
+if (3 < 4) {
+  console.log("This will not not get executed");
+} else if (5 < 7) console.log("5<7 is always right");
+else console.log("This will get executed if both are not true");
+
+if (3 < 4) {
+  console.log("This will not not get executed");
+} else if (5 > 7) console.log("5<7 is always right");
+else console.log("This will get executed if both are not true");
+
+if (3 > 4) {
+  console.log("This will not not get executed");
+} else if (5 > 7) console.log("5<7 is always right");
+else console.log("This will get executed if both are not true");
+
+//income of a person check the income if it is less than 10k, if less than 10k thenyou
+//need to earn more if more than 10k and less than 25k then you are earning for yourself.
+//income greater tha 25k less than 50k then you are eligible to pay tax 7% print the final amount you will earn .
+//income grater than 50k less tha 100k then you are more eligible to pay more tax. tax is 13% print the final amount.
+//if value is negative the salary can be, your salary cannot be negative
+//income greater than 100k and less than 150k then print you are rich you have to  pay 18% tax
